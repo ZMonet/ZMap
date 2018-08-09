c7fe1c5e0e89b9e8e4bf332a034aacda4fd3dd16
ZMonet <2335195449@qq.com>
2018-08-09 10:30:22 +0800

    first commit

---
+++ src/cn/zmonet/entity/UserEntity.java
@@ -0,0 +1,63 @@
+package cn.zmonet.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user", schema = "zmonet", catalog = "")
+public class UserEntity {
+    private int id;
+    private String userName;
+    private String password;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "userName", nullable = false, length = 255)
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Basic
+    @Column(name = "password", nullable = false, length = 255)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserEntity that = (UserEntity) o;
+
+        if (id != that.id) return false;
+        if (userName != null ? !userName.equals(that.userName) : that.userName != null) return false;
+        if (password != null ? !password.equals(that.password) : that.password != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (userName != null ? userName.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
+}
